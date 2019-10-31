f47ae6d187433366fbf0c81dfcaf023d4a4ba4df
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2019-10-31 22:56:49 +0000

    Setting up Environment variables and dumper helper for development

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,18 @@
 <?php
 
-// Start session
+// Start the session.
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// Load any dependencies required for the framework to run
+// Load any dependencies required for the framework to run.
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// Try and load the env variables.
+try {
+    $dotenv = \Dotenv\Dotenv::create(__DIR__ . '/..//');
+    $dotenv->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
+    // If we can't find the .env file and can't read any ENV variable,
+    // then we can't see any of the configuration, so this is safe to stay empty.
+}

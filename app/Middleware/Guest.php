d80f8507e5b91b6a2b7d219f3233c5c216abd8e9
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2019-11-17 22:00:02 +0000

    Adding Guest middleware and refactoring routes

---
+++ app/Middleware/Guest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use App\Auth\Auth;
+
+class Guest implements MiddlewareInterface
+{
+    /**
+     * The auth instence
+     *
+     * @var \App\Auth\Auth
+     */
+    protected $auth;
+
+    /**
+     * Create a new instance
+     *
+     * @param \App\Auth\Auth $auth
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see  \Psr\Http\Server\MiddlewareInterface
+     * @see  https://route.thephpleague.com/4.x/middleware/
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // Invoke the rest of the middleware stack and your controller resulting
+        // in a returned response object
+        $response = $handler->handle($request);
+
+        // If the user is sigend redirect him
+        if ($this->auth->check()) {
+            $response = redirectTo('/');
+        }
+
+        return $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,22 @@ $route->get('/', 'App\Controllers\HomeController::index')->setName('home');
 // Add here any routes for pages require authorization to access them
 $route->group('', function ($route) {
     $route->get('/dashboard', 'App\Controllers\DashboardController::index')->setName('dashboard');
+
+    /* Auth routes - Logout */
+    $route->post('logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
+
 })->middleware($container->get(\App\Middleware\Authenticated::class));
 
-/* Auth routes */
-$route->group('/auth', function ($route) {
+$route->group('', function ($route) {
+
+    /* Auth routes - there's no need for authorized users to access these pages */
 
     /* Login */
     $route->get('/login', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
     $route->post('/login', 'App\Controllers\Auth\LoginController::login');
 
-    /* Logout */
-    $route->post('/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
-
     /* Register */
     $route->get('/register', 'App\Controllers\Auth\RegisterController::index')->setName('auth.register');
     $route->post('/register', 'App\Controllers\Auth\RegisterController::register');
-});
+
+})->middleware($container->get(\App\Middleware\Guest::class));
